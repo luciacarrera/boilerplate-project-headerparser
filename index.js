cbc30878063a86314f7038ba38cebaeb0f9ccff2
Lucía Carrera <luciaacarreraa@gmail.com>
2024-11-10 20:42:43 +0000

    finished request header parser microservice

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 // init project
 require('dotenv').config();
+const fs = require('fs')
 var express = require('express');
 var app = express();
 
@@ -14,15 +15,38 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
+
+
+let whoami= "??"
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  const ipaddress  = req.ip
+  const language = req.headers["accept-language"]        
+  const software = req.headers["user-agent"]        
+  whoami = {ipaddress, language, software}
+  console.log(whoami)
+  res.json(whoami);
 });
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function (req, res) {
+  const absolutePath = __dirname +'/views/index.html'
+  fs.readFile(absolutePath, 'utf8', (err, data) => {
+    if (err) {
+        console.log(err)
+        res.status(500).send('Error reading the HTML file.');
+        return;
+    }
+
+    // Replace the {{name}} placeholder with the dynamic variable
+    const result = data.replace('{{whoami}}', JSON.stringify(whoami));
+
+    // Send the modified HTML content as the response
+    res.send(result);
+  });
+});
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
